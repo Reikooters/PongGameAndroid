aff092b63db3bac76a8de4390477168d923987b1
Shane <reiko@reikooters.net>
2013-09-22 20:29:59 +1000

    Paddles now move a bit better.

--- PongGameAndroid/CppSource/source/InputManager.cpp
+++ PongGameAndroid/CppSource/source/InputManager.cpp
@@ -12,14 +12,14 @@ InputManager::InputManager()
 
 void InputManager::addPointer(const int pointerId, const float x, const float y)
 {
-	if (y < 900.0f) app->getPlayer(0)->moveTo(((x / 1080.0f) - 0.5f) * 1.4f);
-	else if (y > 980.0f) app->getPlayer(1)->moveTo(((x / 1080.0f) - 0.5f) * 1.4f);
+	if (y < 900.0f) app->getPlayer(0)->moveTo(((x / 1080.0f) - 0.5f) * 2.0f);
+	else if (y > 980.0f) app->getPlayer(1)->moveTo(((x / 1080.0f) - 0.5f) * 2.0f);
 }
 
 void InputManager::movePointer(const int pointerId, const float x, const float y)
 {
-	if (y < 900.0f) app->getPlayer(0)->moveTo(((x / 1080.0f) - 0.5f) * 1.4f);
-	else if (y > 980.0f) app->getPlayer(1)->moveTo(((x / 1080.0f) - 0.5f) * 1.4f);
+	if (y < 900.0f) app->getPlayer(0)->moveTo(((x / 1080.0f) - 0.5f) * 2.0f);
+	else if (y > 980.0f) app->getPlayer(1)->moveTo(((x / 1080.0f) - 0.5f) * 2.0f);
 }
 
 void InputManager::removePointer(const int pointerId)
